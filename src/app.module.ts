6dc5537e576ba00516ecd4092cac84532d41d377
Hyeonkwon Cho <chkwon91@gmail.com>
2022-11-30 16:32:20 +0900

    feature: controller 연습

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudyController } from './study/study.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, StudyController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/study/study.controller.ts
@@ -0,0 +1,41 @@
+/* https://docs.nestjs.com/controllers */
+
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+export class StudyDto {
+  name: string;
+  age: number;
+  breed: string | null;
+}
+
+@Controller('study')
+export class StudyController {
+  @Get('get')
+  getHelp(
+    @Res() res: Response,
+    @Query('name') name: string,
+    @Query('age') age: number,
+  ) {
+    res.status(HttpStatus.OK).json({
+      hello: 'world',
+      name: name,
+      age: age,
+    });
+  }
+
+  @Post('post')
+  create(@Body() dto: StudyDto, @Res() res: Response) {
+    res.status(HttpStatus.OK).json({
+      dto: dto,
+    });
+  }
+}
