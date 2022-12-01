02e0141bbf6ed8b0dc54bbbdfd47574f3bc5fbbb
Hyeonkwon Cho <chkwon91@gmail.com>
2022-12-01 15:45:50 +0900

    feature: transformer 작업

---
+++ src/transform/transform.interceptor.ts
@@ -0,0 +1,29 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable, tap } from 'rxjs';
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => {
+        return {
+          data: data, // 컨트롤러에서 res.status() 로 보내주면 안됨.
+        };
+      }),
+    );
+  }
+}
